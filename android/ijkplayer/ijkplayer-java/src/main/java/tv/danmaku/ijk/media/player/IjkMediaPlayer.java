24ed99c60869f84f08a6883eab4a048b424af803
wuzhiqiang <wuzhiqiang@bilibili.com>
2018-03-22 17:22:28 +0800

    ijkplayer: add rotate check for mp4

--- android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ android/ijkplayer/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -148,7 +148,7 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
     public static final int FFP_PROP_INT64_LATEST_SEEK_LOAD_DURATION        = 20300;
     public static final int FFP_PROP_INT64_IMMEDIATE_RECONNECT              = 20211;
     public static final int FFP_PROP_INT64_HW_DECODEC_ERROR_CODE            = 20212;
-    public static final int FFP_PROP_INT64_EXTRADATA_ERROR                  = 20213;
+    public static final int FFP_PROP_INT64_ASYNC_ERROR_CODE                 = 20213;
     //----------------------------------------
 
     public static final int PLAYER_ACTION_IS_INIT    = 10001;
@@ -1719,6 +1719,16 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
         return 0;
     }
 
+    public long getAsyncInitDecoderErrorCode() {
+        if (mPlayer != null && mServiceIsConnected) {
+            try {
+                return mPlayer.getPropertyLong(FFP_PROP_INT64_ASYNC_ERROR_CODE, 0);
+            } catch (RemoteException e) {
+                onBuglyReport(e);
+            }
+        }
+        return 0;
+    }
     public long getVideoCachedPackets() {
         if (mPlayer != null && mServiceIsConnected) {
             try {
@@ -1849,17 +1859,6 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
             }
         }
         return 0;
-    }
-
-    public boolean isExtradataError() {
-        if (mPlayer != null && mServiceIsConnected) {
-            try {
-                return mPlayer.getPropertyLong(FFP_PROP_INT64_EXTRADATA_ERROR, 0) != 0;
-            } catch (RemoteException e) {
-                onBuglyReport(e);
-            }
-        }
-        return false;
     }
 
     public long getBitRate() {
